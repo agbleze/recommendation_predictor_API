becd2079dda009d3bfa01e8b31af7362b95ca273
agbleze <lagbleze@st.ug.edu.gh>
2023-01-25 21:07:14 +0100

    Update vectorizer.py

--- vectorizer.py
+++ vectorizer.py
@@ -27,57 +27,57 @@ from data_preprocess import preprocess_texts_to_tokens
 #%% Vectorizer
 class ReviewVectorizer(object):
     """coordinate the vocabulary and put them to use"""
-    def __init__(self, title_vocab, category_vocab) -> None:
-        self.title_vocab = title_vocab
+    def __init__(self, review_vocab, category_vocab) -> None:
+        self.review_vocab = review_vocab
         self.category_vocab = category_vocab
         
-    def vectorize(self, title, vector_length=-1):
-        indices = [self.title_vocab.begin_seq_index]
-        indices.extend(self.title_vocab.lookup_token(token) for token in preprocess_texts_to_tokens(title))
-        indices.append(self.title_vocab.end_seq_index)
+    def vectorize(self, review, vector_length=-1):
+        indices = [self.review_vocab.begin_seq_index]
+        indices.extend(self.review_vocab.lookup_token(token) for token in preprocess_texts_to_tokens(review))
+        indices.append(self.review_vocab.end_seq_index)
         
         if vector_length < 0:
             vector_length = len(indices)
             
         out_vector = np.zeros(vector_length, dtype=np.int64)
         out_vector[:len(indices)] = indices
-        out_vector[len(indices):] = self.title_vocab.mask_index
+        out_vector[len(indices):] = self.review_vocab.mask_index
         
         return out_vector
     
     @classmethod
-    def from_dataframe(cls, news_df, cutoff=0):
+    def from_dataframe(cls, review_df, cutoff=0):
         category_vocab = Vocabulary()
-        for category in sorted(set(news_df['reviews.doRecommend'])):
+        for category in sorted(set(review_df['reviews.doRecommend'])):
             category_vocab.add_token(category)
             
         word_counts = Counter()
-        for title in news_df['reviews.text']:
+        for review in review_df['reviews.text']:
             # for token in title.split(" "):
             #     if token not in string.punctuation:
             #         word_counts[token] += 1
             
-            for token in preprocess_texts_to_tokens(title):
+            for token in preprocess_texts_to_tokens(review):
                 word_counts[token] += 1
                     
-        title_vocab = SequenceVocabulary()
+        review_vocab = SequenceVocabulary()
         for word, word_count in word_counts.items():
             if word_count >= cutoff:
-                title_vocab.add_token(word)
+                review_vocab.add_token(word)
                 
-        return cls(title_vocab, category_vocab)
+        return cls(review_vocab, category_vocab)
     
     @classmethod
     def from_serializable(cls, contents):
-        title_vocab = SequenceVocabulary.from_serializable(contents['title_vocab'])
+        review_vocab = SequenceVocabulary.from_serializable(contents['title_vocab'])
         
         category_vocab = Vocabulary.from_serializable(contents['category_vocab'])
         
-        return cls(title_vocab=title_vocab, category_vocab=category_vocab)
+        return cls(review_vocab=review_vocab, category_vocab=category_vocab)
     
     #@classmethod
     def to_serializable(self):
-        return {'title_vocab': self.title_vocab.to_serializable(),
+        return {'review_vocab': self.review_vocab.to_serializable(),
                 'category_vocab': self.category_vocab.to_serializable()}
     
 
