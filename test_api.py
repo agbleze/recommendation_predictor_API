09d66597f41a5e7e3087df3964b99faf0a18d029
agbleze <lagbleze@st.ug.edu.gh>
2023-03-07 20:41:26 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -10,11 +10,23 @@ import requests
 #requests.get(url='http://127.0.0.1:8000')
 
 # %%
-res = requests.post(url='https://f696-138-246-3-71.ngrok.io/predict', json={'review': 'I recommend product'})
+res = requests.post(url='https://fbfb-138-246-3-71.ngrok.io/predict', 
+                    json={'review': 'recommend product'})
 
-res.content
+print(res.content)
 
 
+#https://bewjff2ygzhmhe7luyxlbgk3am.srv.us/
 
-
+# %%
+import json
+def request_prediction(URL: str, review_data: str):
+    in_data = {'review': review_data}
+    req = requests.post(url = URL, json=in_data)
+    response = req.content
+    prediction = json.loads(response)#['category']
+    return prediction
+  
+# %%
+request_prediction(URL='https://bewjff2ygzhmhe7luyxlbgk3am.srv.us/predict', review_data='recommend product')
 # %%
