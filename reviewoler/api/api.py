bbc55f0da58c28ce75dc2afed3e182efdb03233c
linus <lagbleze@st.ug.edu.gh>
2025-07-15 01:37:05 +0200

    feat: change to useoptimized onnx model

--- reviewoler/api/api.py
+++ reviewoler/api/api.py
@@ -1,48 +1,30 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from ..utils.helpers import predict_category
-from ..model.classifier import ReviewClassifier
-import torch
 import json
 from ..utils.helpers import (args, predict_category)
 from ..preprocess.vectorizer import ReviewVectorizer
-from ..utils.embedding_matrix import EmbeddingMatrixMaker
-from ..model_store.artefacts import embedding_path
+import onnxruntime as ort
 
 file = open(args.vectorizer_file)
 vectorizer_file = json.load(file)
 
-review_vocab = vectorizer_file['title_vocab']['token_to_idx'].keys()
+review_vocab = vectorizer_file['review_vocab']['token_to_idx'].keys()
 review_outcome = vectorizer_file['category_vocab']['token_to_idx']  
 
-embedding_matrix = EmbeddingMatrixMaker(glove_filepath=args.glove_filepath,
-                                        words=review_vocab
-                                    )
 
-embeddings = embedding_matrix.load_embedding(embedding_path)
-classifier = ReviewClassifier(embedding_size=args.embedding_size,
-                            num_embeddings=len(review_vocab),
-                            num_channels=args.num_channels,
-                            hidden_dim=args.hidden_dim,
-                            num_classes=len(review_outcome),
-                            dropout_p=args.dropout_p,
-                            pretrained_embeddings=embeddings,
-                            padding_idx=0
-                            )
-
-classifier.load_state_dict(torch.load(args.model_state_file))
-
-classifier = classifier.to(args.device)
 vectorizer = ReviewVectorizer.from_serializable(contents=vectorizer_file)
+session = ort.InferenceSession(args.model_file)
+
 class RecommendPredictor(Resource):
     @staticmethod
     def post():
         review = request.get_json()['review']
         
-        result = predict_category(review=review, classifier=classifier,
-                         vectorizer=vectorizer, 
-                         max_length=args.max_seq_length + 2 # +2 for the begin and end sequence tokens
-                         )       
+        result = predict_category(review=review, session=session,
+                                vectorizer=vectorizer, 
+                                max_length=args.max_seq_length + 2 # +2 for the begin and end sequence tokens
+                                )       
         return result
     
 class Entrypoint(Resource):
