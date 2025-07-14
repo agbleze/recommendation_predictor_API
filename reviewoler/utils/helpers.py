2c0d6e6a597c90471f06a45bea025ee8ce7981bb
linus <lagbleze@st.ug.edu.gh>
2025-07-14 23:56:15 +0200

    remove func

--- reviewoler/utils/helpers.py
+++ reviewoler/utils/helpers.py
@@ -98,12 +98,7 @@ def compute_accuracy(y_pred, y_target):
     n_correct = torch.eq(y_pred_indices, y_target).sum().item()
     return n_correct / len(y_pred_indices) * 100        
         
-def set_seed_everywhere(seed, cuda):
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    if cuda:
-        torch.cuda.manual_seed_all(seed)
-        
+       
 def handle_dirs(dirpath):
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
